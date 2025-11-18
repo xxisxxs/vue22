4001d0d79cf005cc5b8cbedf73a65c69cea50ade
xiaoliziaaa <1756329434@qq.com>
2025-11-18 10:44:11 +0800

    222

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <div id="map" style="width: 100%; height: 100%"></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
